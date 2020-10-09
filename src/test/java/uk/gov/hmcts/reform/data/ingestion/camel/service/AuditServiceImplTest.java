a0420672927076b70d8426c5e1c6111a0a441946
shreedhar-hmcts <ed71fae8f60e250eedd92483bcf497c20a22ce95>
2020-10-09 09:20:46 +0100

    RDCC-1903 added junit

---
+++ src/test/java/uk/gov/hmcts/reform/data/ingestion/camel/service/AuditServiceImplTest.java
@@ -0,0 +1,91 @@
+package uk.gov.hmcts.reform.data.ingestion.camel.service;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.util.ReflectionTestUtils.setField;
+import static uk.gov.hmcts.reform.data.ingestion.camel.util.MappingConstants.SCHEDULER_NAME;
+import static uk.gov.hmcts.reform.data.ingestion.camel.util.MappingConstants.SCHEDULER_START_TIME;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockitoAnnotations;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AuditServiceImplTest {
+
+    JdbcTemplate mockJdbcTemplate = mock(JdbcTemplate.class);
+    AuditServiceImpl dataLoadAuditUnderTest = new AuditServiceImpl();
+    PlatformTransactionManager platformTransactionManager = mock(PlatformTransactionManager.class);
+    TransactionStatus transactionStatus = mock(TransactionStatus.class);
+
+    Exchange exchange = mock(Exchange.class);
+    CamelContext camelContext = mock(CamelContext.class);
+
+    final String schedulerName = "judicial_main_scheduler";
+
+
+
+    public static Map<String, String> getGlobalOptions(String schedulerName) {
+        Map<String, String> globalOptions = new HashMap<>();
+        globalOptions.put("ORCHESTRATED_ROUTE", "JUDICIAL_REF_DATA_ORCHESTRATION");
+        globalOptions.put(SCHEDULER_START_TIME, String.valueOf(new Date().getTime()));
+        globalOptions.put(SCHEDULER_NAME, schedulerName);
+        return globalOptions;
+    }
+
+    @Before
+    public void setUp() {
+        setField(dataLoadAuditUnderTest, "jdbcTemplate", mockJdbcTemplate);
+        setField(dataLoadAuditUnderTest, "platformTransactionManager", platformTransactionManager);
+        setField(dataLoadAuditUnderTest,"invalidExceptionSql", "select * from appointment");
+        //jdbcTemplate
+
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testSchedulerAuditUpdate() throws Exception {
+
+        when(exchange.getContext()).thenReturn(camelContext);
+        Map<String, String> globalOptions = getGlobalOptions(schedulerName);
+        when(exchange.getContext().getGlobalOptions()).thenReturn(globalOptions);
+        when(camelContext.getGlobalOptions()).thenReturn(globalOptions);
+        when(mockJdbcTemplate.update(anyString(), anyString(), any(), any(), any())).thenReturn(1);
+        when(platformTransactionManager.getTransaction(any())).thenReturn(transactionStatus);
+        doNothing().when(platformTransactionManager).commit(transactionStatus);
+
+        dataLoadAuditUnderTest.auditSchedulerStatus(camelContext);
+
+        verify(exchange, times(1)).getContext();
+        verify(camelContext, times(1)).getGlobalOptions();
+        verify(platformTransactionManager, times(1)).getTransaction(any());
+    }
+
+    @Test
+    public void testAuditException() throws Exception {
+
+        when(exchange.getContext()).thenReturn(camelContext);
+        Map<String, String> globalOptions = getGlobalOptions(schedulerName);
+        when(exchange.getContext().getGlobalOptions()).thenReturn(globalOptions);
+        when(camelContext.getGlobalOptions()).thenReturn(globalOptions);
+        when(mockJdbcTemplate.update(anyString(), any(), any())).thenReturn(1);
+        when(platformTransactionManager.getTransaction(any())).thenReturn(transactionStatus);
+        doNothing().when(platformTransactionManager).commit(transactionStatus);
+        dataLoadAuditUnderTest.auditException(camelContext, "exceptionMessage", true);
+        verify(exchange, times(1)).getContext();
+        verify(camelContext, times(1)).getGlobalOptions();
+        verify(platformTransactionManager, times(1)).getTransaction(any());
+    }
+}
