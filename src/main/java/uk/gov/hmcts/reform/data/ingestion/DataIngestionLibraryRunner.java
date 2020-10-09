ec7b5237dcb5c6abeb826913b84fbc243d67b594
AdilOoze <adil.oozeerally@hmcts.net>
2020-10-09 13:48:23 +0100

    RDCC-1902: added runner class

---
+++ src/main/java/uk/gov/hmcts/reform/data/ingestion/DataIngestionLibraryRunner.java
@@ -0,0 +1,33 @@
+package uk.gov.hmcts.reform.data.ingestion;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.data.ingestion.camel.service.AuditServiceImpl;
+
+import static java.lang.Boolean.FALSE;
+
+@Slf4j
+@Component
+public class DataIngestionLibraryRunner {
+
+    @Autowired
+    JobLauncher jobLauncher;
+
+    @Autowired
+    AuditServiceImpl auditServiceImpl;
+
+    public void run(Job job, JobParameters params) throws Exception {
+
+        if (FALSE.equals(auditServiceImpl.isAuditingCompleted())) {
+            log.info("Data Ingestion Library running first time for a day::");
+            jobLauncher.run(job, params);
+            log.info("Data Ingestion Library job run completed::");
+        } else {
+            log.info("no run of Data Ingestion Library as it has ran for the day::");
+        }
+    }
+}
