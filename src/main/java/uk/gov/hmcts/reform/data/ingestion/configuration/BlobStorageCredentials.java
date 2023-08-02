b872e3f82b6858a7d128c52f8f8d85fad59424d8
Lukasz Wolski <1005015+lukasz-wolski@users.noreply.github.com>
2023-08-02 16:32:19 +0100

    review comments

--- src/main/java/uk/gov/hmcts/reform/data/ingestion/configuration/BlobStorageCredentials.java
+++ src/main/java/uk/gov/hmcts/reform/data/ingestion/configuration/BlobStorageCredentials.java
@@ -1,7 +1,7 @@
 package uk.gov.hmcts.reform.data.ingestion.configuration;
 
+import com.azure.storage.blob.BlobServiceClientBuilder;
 import com.azure.storage.common.StorageSharedKeyCredential;
-//import com.microsoft.azure.storage.CloudStorageAccount;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -20,6 +20,11 @@ public class BlobStorageCredentials {
     @Bean(name = "credsreg")
     public StorageSharedKeyCredential credentials() {
         return new StorageSharedKeyCredential(azureBlobConfig.getAccountName(), azureBlobConfig.getAccountKey());
+    }
+
+    @Bean(name = "clientBuilder")
+    public BlobServiceClientBuilder clientBuilder() {
+        return new BlobServiceClientBuilder();
     }
 
 }
